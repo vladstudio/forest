20a7b1552390cf42f0f534960d202b88fbd18bf4
Vlad Gerasimov <vlad@vlad.studio>
2026-04-16 22:01:12 +0400

    Clean up extension and artifacts before building

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+code --uninstall-extension vladstudio.vladstudio-forest
+rm vladstudio-forest-*.vsix
 bun run package
 vsce package
 code --install-extension vladstudio-forest-*.vsix
